4ad9f4ddae5ce5ed51c5679fd4549090e00da5db
terminator1333 <lenga@post.bgu.ac.il>
2025-04-22 18:22:16 +0300

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void ThreadTest()
+        {
+            
+            Console.WriteLine("Hellow, world" + new Random().Next());
+
+        }
+        static void Main(string[] args)
+        {
+            Stopwatch stop = Stopwatch.StartNew();
+            Thread thread = new Thread(ThreadTest);
+            thread.Start();
+
+            stop.Stop();
+            Console.WriteLine($"Time taken: {stop.Elapsed.TotalMilliseconds} ms");
+
+        }
+    }
+    class MatrixMultiplier
+    {
+        public static MatrixMultiplierThread()
+        public static void MultiplyMatricesConcurrently(int[,]matrixA, int[,]matrixB, int[,] resultMatrix, int rowsA, int colsA, int colsB, int numThreads)
+        {
+            int amountPerThread = rowsA / numThreads;
+
+            if (amountPerThread * numThreads != rowsA)
+                amountPerThread++;
+            Thread[] threads = new Thread[amountPerThread];
+
+
+        }
+    }
+}
